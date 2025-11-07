599262f092376ff0c17d4de55e02ea6b8dc1b825
Guilherme Macedo <macedo@dca.fee.unicamp.br>
2025-11-06 22:04:30 -0300

    Update livro.py

--- program/livro.py
+++ program/livro.py
@@ -5,3 +5,13 @@ class Livro:
         self.isbn  = isbn
         self.nome  = nome
         self.autor = autor
+
+    def __eq__(self, isbn):
+        
+        return self.isbn == isbn
+    
+    def exportar(self, caminho):
+
+        with open(caminho + '/' + self.nome + '.txt', 'w') as file:
+            
+            file.write(f'{self.nome} - {self.autor.nome}')
